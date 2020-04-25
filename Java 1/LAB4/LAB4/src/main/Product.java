aa1dcbc4d40a537bc2bb38ffeb83f1bc87410194
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-04-26 00:41:42 +0700

    create a class, generate constructor, setter, getter

---
+++ Java 1/LAB4/LAB4/src/main/Product.java
@@ -0,0 +1,63 @@
+package main;
+
+import java.util.Scanner;
+
+public class Product {
+
+    public void showProduct() {
+        System.out.println("Product name: " + this._productName);
+        System.out.println("Unit price: " + this._unitPrice);
+        System.out.println("Discount: " + this._discount);
+    }
+
+    public void input() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter product name: ");
+        this._productName = sc.nextLine();
+        System.out.print("Enter unit price: ");
+        this._unitPrice = sc.nextDouble();
+        System.out.print("Enter discount: ");
+        this._discount = sc.nextDouble();
+        // sc.close();
+    }
+
+    public String get_productName() {
+        return _productName;
+    }
+
+    public void set_productName(String _productName) {
+        this._productName = _productName;
+    }
+
+    public double get_unitPrice() {
+        return _unitPrice;
+    }
+
+    public void set_unitPrice(double _unitPrice) {
+        this._unitPrice = _unitPrice;
+    }
+
+    public double get_discount() {
+        return _discount;
+    }
+
+    public void set_discount(double _discount) {
+        this._discount = _discount;
+    }
+
+    Product(String productName, double unitPrice, double discount) {
+        this._productName = productName;
+        this._unitPrice = unitPrice;
+        this._discount = discount;
+    }
+
+    Product(String productName, double unitPrice) {
+        this(productName, unitPrice, 0);
+    }
+
+    Product(){}
+
+    private String _productName;
+    private double _unitPrice;
+    private double _discount;
+}
