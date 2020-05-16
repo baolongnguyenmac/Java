66ecbb54062933ccdccaa0620aa9e36f75e717a5
Nguyễn Bảo Long <nguyenbaolong@MacBook-Pro-cua-Nguyen.local>
2020-05-16 15:45:02 +0700

    delete shit

--- Deadline 2/Main.java
+++
@@ -1,9 +0,0 @@
-// import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        School school = new School();
-        Menu m = new Menu();
-        m.executeMenu(school);
-    }
-}
