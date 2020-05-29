97359157c0a15def03077c95d1ab0c863c1676f9
Nguyễn Bảo Long <nguyenbaolong@MEO-MEO.mshome.net>
2020-05-29 16:36:13 +0700

    change name

--- Deadline 2/Dictionary/src/test/TestReadWriteDictionary.java
+++ Deadline 2/Dictionary/src/test/TestReadWriteDictionary.java
@@ -3,7 +3,7 @@ package test;
 import com.java.handlefile.*;
 import com.java.model.*;
 
-public class TestRead {
+public class TestReadWriteDictionary {
     public static void main(String[] args) {
         Reader r = new Reader();
         r.readDictionary();
